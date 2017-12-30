093be0e1e2debbd4ec6a72c46d334a411bd19487
Yaw <yaw@github.com>
2017-12-30 15:25:05 +0800

    自定义starter引入

--- src/main/java/com/yaw/DemoApplication.java
+++ src/main/java/com/yaw/DemoApplication.java
@@ -1,20 +1,17 @@
 package com.yaw;
 
-import com.yaw.aop.AopTest;
-import com.yaw.datasource.TestDao;
+import com.yaw.redis.EnableRedis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import redis.clients.jedis.Jedis;
 
 /**
  * spring.datasource.type 可以指定哪种数据源，默认支持tomcat (共Tomcat,Hikari,Dbcp,Dbcp2,Generic 五种）
  * jdbcTemplate，执行语句插入数据
  */
+@EnableRedis
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class DemoApplication {
     public static void main(String[] args) throws Exception {
@@ -34,6 +31,12 @@ public class DemoApplication {
         /*
          *aop
          */
-        context.getBean(AopTest.class).create("aaaa","12121");
+//        context.getBean(AopTest.class).create("aaaa","12121");
+        /*
+         * 自定义springbootstarter
+         */
+        Jedis jedis = context.getBean(Jedis.class);
+        jedis.set("id","1");
+        System.out.println(jedis.get("id"));
     }
 }
